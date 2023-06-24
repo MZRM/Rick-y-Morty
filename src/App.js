ada8340cbed3ce92f43f7e88341b323bec060ca3
MZRM <eingencomzrm@hotmail.com>
2023-06-23 20:04:04 -0600

    firts commit rick

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import imagen1 from '../src/img/rick-mortty.png'
+import { useState } from 'react';
+import './App.css';
+import Characters from './components/Characters';
+
+function App() {
+  const [characters, setCharacters] = useState(null);
+
+  const reqApi = async() => {
+    const api = await fetch("https://rickandmortyapi.com/api/character")
+    console.log(api)
+
+
+    const characterApi = await api.json();
+      console.log(characterApi)
+      console.log(characters);
+      setCharacters(characterApi);
+      console.log(characters);
+    //console.log("clicking");
+  }
+  return (
+    <div className="App">
+
+    <header className="App-header">
+
+    <h1 className='title'>Rick & Morty</h1>
+
+    {characters ? (
+    <Characters characters ={characters}></Characters>
+):(
+  <>
+    <img src={imagen1}alt='Rick & Morty' className='img-home'/>
+    <button onClick={reqApi}className='btn-search'>
+      Buscar personajes
+    </button>
+</>
+)}
+    </header>
+
+    </div>
+  );
+}
+
+export default App;
